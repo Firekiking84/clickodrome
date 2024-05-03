50feb75989238ea7074558fe107d4c91c597a8a6
Firekiking <kgalaxie84@gmail.com>
2024-05-03 16:24:29 +0200

    Ajout sécurité

--- include/div.h
+++ include/div.h
@@ -24,27 +24,6 @@ typedef struct			s_div
   t_vector_ptr			*pictures;
   t_vector_ptr			*timers;
 }				t_div;
-<<<<<<< Updated upstream
-t_div				*efnew_div(const char			*name,
-					   t_zposition		        *pos,
-					   t_bunny_size			size);
-void				efdelete_div(t_div			*div);
-void				efdisplay_div(t_div			*div,
-					      t_bunny_pixelarray	*px);
-t_button			*efadd_button_div(t_div			*div,
-						 t_button_settings	*set);
-t_label				*efadd_label_div(t_div	 		*div,
-						t_label_settings	*set);
-t_text_box   			*efadd_text_box_div(t_div		*div,
-						   t_textbox_settings	*set);
-t_picture			*efadd_picture_div(t_div		*div,
-						  t_picture_settings	*set);
-t_timer				*efadd_timer_div(t_div			*div,
-						t_timer_settings	*set);
-void			        efevents_div(t_div                      *div,
-					     const t_bunny_event        *event);
-#endif //		__DIV_H__		//
-=======
 
 t_div				*efnew_div(const char				*name,
 					   t_zposition				*pos,
@@ -69,4 +48,3 @@ bool				efcheck_pos_new_component(t_div			*div,
 							  t_bunny_size		*size);
 
 #endif //	__DIV_H__		//
->>>>>>> Stashed changes
