37ef72ea3c9b152e5312b8046e54319dcd542b06
romain.cescutti <romain.cescutti@gagarine.efrits.fr>
2024-05-06 13:39:53 +0200

    fixed size_t issue with get_div_by_name

--- include/button.h
+++ include/button.h
@@ -46,7 +46,6 @@ typedef struct		s_button_settings
 t_button		*efnew_button(t_button_settings *set);
 void			efdelete_button(t_button		*button);
 t_button		*efnew_button(t_button_settings		*set);
-size_t			efdelete_button(t_button		*button);
 void			efdisplay_button(t_button		*button,
 					 t_bunny_zpixelarray	*px);
 void			efevents_button(t_button	    	*button,
